9719f4e490d05ca8587ea6bd24e3dce55b80787e
bellapense <fg4529@wayne.edu>
2020-09-04 18:17:54 -0400

    You can type in word!

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
 import React from "react"
 import useGameLogic from "./hooks/useGameLogic"
+import GameDisplay from "./components/GameDisplay";
 import TimeSelector from "./components/TimeSelector"
 
 import "./styles/App.css"
 
 function App() {
-  const {
-    textareaRef,
-    handleChange,
-    text,
-    isTimeRunning,
-    timeRemaining,
-    updateGameLength,
-    startGame,
-    wordCount
-  } = useGameLogic()
+    const {
+        textareaRef,
+        handleChange,
+        text,
+        isTimeRunning,
+        timeRemaining,
+        updateGameLength,
+        startGame,
+        wordCount
+    } = useGameLogic()
 
-  return (
-      <div>
-        <h1>How fast do you type?</h1>
-        <textarea
-            onChange={handleChange}
-            value={text}
-            disabled={!isTimeRunning}
-            ref={textareaRef}
-        />
-        <h4>Time remaining: {timeRemaining}</h4>
-        <TimeSelector
-            updateGameLength={updateGameLength}
-            isTimeRunning={isTimeRunning}
-        />
-        <button
-            onClick={startGame}
-            disabled={isTimeRunning}
-        >
-          Start
-        </button>
-        <h1>Word count: {wordCount}</h1>
-      </div>
-  )
+    const word = "WORD"  //TODO: Move to logic hook and randomize
+
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <GameDisplay
+                word={word}
+            />
+            <textarea
+                hidden={true}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+                ref={textareaRef}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <TimeSelector
+                updateGameLength={updateGameLength}
+                isTimeRunning={isTimeRunning}
+            />
+            <button
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >
+                Start
+            </button>
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/GameDisplay.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from "react"
+
+function GameDisplay(props) {
+    const charsToMatch = props.word.split("")
+    const [charEntered, setCharEntered] = useState("")
+    const [charsMatched, setCharsMatched] = useState([])
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.addEventListener('keydown', event => {
+            const charList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            const key = event.key.toUpperCase()
+
+            if (charList.indexOf(key) === -1) return
+            setCharEntered(key)
+        })
+    })
+
+    useEffect(() => {
+        if (charEntered === charsToMatch[charsMatched.length]) {
+            setCharsMatched(prevCharsMatched => [...prevCharsMatched, charEntered])
+        }
+
+        setCharEntered("")
+    }, [charEntered, charsMatched, charsToMatch])
+
+    const word = (charsToMatch.map((char, index) => {
+        let isMatched = ""
+        if ((index + 1) <= charsMatched.length) {
+            isMatched = "matched"
+        }
+        return <span key={index} className={isMatched}>{char}</span>
+    }))
+
+    return (
+        <div id="game-display">
+            <p id="the-word">
+                {word}
+            </p>
+        </div>
+    )
+}
+
+export default GameDisplay
