f7e8eba28858d70527dcd8081eaabf2bb5a6d60f
bellapense <fg4529@wayne.edu>
2020-09-04 12:59:15 -0400

    Updates

---
+++ precache-manifest.49b82fd438f304656a21a35fd663fb95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d9b4f3a18ec7c0870daf1df3471c0f",
+    "url": "/speed-typing-game/index.html"
+  },
+  {
+    "revision": "f7bb83a2ceeaf85a5c64",
+    "url": "/speed-typing-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f7ec7aed2bab5503333a",
+    "url": "/speed-typing-game/static/js/2.8fecb7be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-typing-game/static/js/2.8fecb7be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bb83a2ceeaf85a5c64",
+    "url": "/speed-typing-game/static/js/main.0fdb91ff.chunk.js"
+  },
+  {
+    "revision": "294e6ac9cd4f7e7f2592",
+    "url": "/speed-typing-game/static/js/runtime-main.b1de8fe3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/speed-typing-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
